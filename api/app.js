e93bec00c06338e0901c11d704c6023dfed674fc
Iván Rodríguez <82571096+Ivan180687@users.noreply.github.com>
2022-10-11 21:38:56 +0200

    fix: fixed bug on CORS Methods

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use((req, res, next) => {
   res.set("Access-Control-Allow-Origin", "http://localhost:3000");
   res.set("Access-Control-Allow-Headers", "content-type");
-  res.set("Access-Control-Allow-Methods", "*");
+  res.set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   res.set("Access-Control-Allow-Credentials", "true");
   next();
 })
