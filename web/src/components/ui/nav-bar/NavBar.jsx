93c4c28e01d922a0ffa3445c427008222847532c
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-10-16 14:28:42 +0200

    fix: menus hidden on navbar when user is not authenticated

--- web/src/components/ui/nav-bar/NavBar.jsx
+++ web/src/components/ui/nav-bar/NavBar.jsx
@@ -1,37 +1,37 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../../contexts/AuthContext';
-import { userLogout } from '../../../services/offer-user-service';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { userLogout } from "../../../services/offer-user-service";
 
-const pages = ['New Offer', 'Create Service'];
-const settings = ['Profile', 'Logout'];
+const pages = ["New Offer", "Create Service"];
+const settings = ["Profile", "Logout"];
 
 const NavBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const user = React.useContext(AuthContext)
+  const user = React.useContext(AuthContext);
 
   const handleClick = () => {
     userLogout()
       .then(() => {
         console.log("session finished");
-        user.logOut()     
+        user.logOut();
       })
-      .catch((error) => console.error(error))
-  }
+      .catch((error) => console.error(error));
+  };
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -52,7 +52,9 @@ const NavBar = () => {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <RocketLaunchIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <RocketLaunchIcon
+            sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+          />
           <Typography
             variant="h6"
             noWrap
@@ -60,116 +62,151 @@ const NavBar = () => {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             LOGO
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >              
-              <MenuItem onClick={handleCloseNavMenu}>
-                <Typography textAlign="center" component={Link} to="/offers/create" className="text-decoration-none text-dark">{pages[0]}</Typography>
-              </MenuItem>
-              <MenuItem onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center" component={Link} to="/services/create" className="text-decoration-none text-dark">{pages[1]}</Typography>
+          {user.user && (
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <Typography
+                    textAlign="center"
+                    component={Link}
+                    to="/offers/create"
+                    className="text-decoration-none text-dark"
+                  >
+                    {pages[0]}
+                  </Typography>
                 </MenuItem>
-            </Menu>
-          </Box>
-          <RocketLaunchIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <Typography
+                    textAlign="center"
+                    component={Link}
+                    to="/services/create"
+                    className="text-decoration-none text-dark"
+                  >
+                    {pages[1]}
+                  </Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+          )}
+          <RocketLaunchIcon
+            sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+          />
           <Typography
             component={Link}
             to="/"
-            variant="h5"
+            variant="h6"
             noWrap
             href=""
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              fontFamily: "verdana",
+              fontWeight: 550,
+              letterSpacing: ".2rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
-            Shipping Bids
+            SHIPPING BIDS
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: "white", display: "block" }}
               >
                 {page}
               </Button>
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src={`${user?.user?.image}` || ""} />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              <MenuItem onClick={handleCloseUserMenu}>
-                <Typography textAlign="center" component={Link} to={`/users/${user?.user?.id}` || ""} className="text-decoration-none text-dark">{settings[0]}</Typography>
-              </MenuItem>
-              <MenuItem onClick={handleCloseUserMenu}>
-                <Typography textAlign="center" className="text-decoration-none text-danger" onClick={handleClick}>{settings[1]}</Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
+          {user.user && (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src={`${user?.user?.image}` || ""} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography
+                    textAlign="center"
+                    component={Link}
+                    to={`/users/${user?.user?.id}` || ""}
+                    className="text-decoration-none text-dark"
+                  >
+                    {settings[0]}
+                  </Typography>
+                </MenuItem>
+                <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography
+                    textAlign="center"
+                    className="text-decoration-none text-danger"
+                    onClick={handleClick}
+                  >
+                    {settings[1]}
+                  </Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
@@ -177,17 +214,10 @@ const NavBar = () => {
 };
 export default NavBar;
 
-
-
-
-
-
-
 // import React, { useContext } from 'react'
 // import { Link, NavLink} from 'react-router-dom'
 // import { AuthContext } from '../../../contexts/AuthContext'
 // import { userLogout } from '../../../services/offer-user-service'
-
 
 // function NavBar() {
 //   const user = useContext(AuthContext)
@@ -196,13 +226,13 @@ export default NavBar;
 //     userLogout()
 //       .then(() => {
 //         console.log("session finished");
-//         user.logOut()     
+//         user.logOut()
 //       })
 //       .catch((error) => console.error(error))
 //   }
 //   return (
 //     <>
-    
+
 //       <nav className="navbar navbar-expand-lg bg-light">
 //         <div className="container-fluid">
 //           <Link className="navbar-brand" to="/">Shipping Bids</Link>
