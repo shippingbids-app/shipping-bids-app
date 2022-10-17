71605d7899200acfa1394af2e6c96cccbb81879c
Iván <ivantbd87@gmail.com>
2022-10-17 21:53:20 +0200

    Feat: created scroll up button

--- web/src/screens/offers-screens/OffersListScreen.jsx
+++ web/src/screens/offers-screens/OffersListScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import OfferCard from "../../components/offers/offer-card/OfferCard";
+import ScrollUp from "../../components/ui/scroll-up/ScrollUp";
 import * as offerService from "../../services/offer-user-service";
 
 function OffersListScreen() {
@@ -14,6 +15,7 @@ function OffersListScreen() {
   
   return (
     <div className="container">
+      <ScrollUp />
       {offers?.map((offer) => (
         <OfferCard {...offer} key={offer.id} />
       ))}
